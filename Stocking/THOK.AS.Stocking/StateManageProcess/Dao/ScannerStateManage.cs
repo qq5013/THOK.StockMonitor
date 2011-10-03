cbc016c54cef9e9794ef77f68910e26c4d1b4442
wuyl <981145751@qq.com>
2011-10-03 16:52:59 +0800

    修改LED和扫码器状态管理器的日志显示位置 Signed-off-by: wuyl <981145751@qq.com>

--- Stocking/THOK.AS.Stocking/StateManageProcess/Dao/ScannerStateManage.cs
+++ Stocking/THOK.AS.Stocking/StateManageProcess/Dao/ScannerStateManage.cs
@@ -125,7 +125,6 @@ namespace THOK.AS.Stocking.StateManageProcess.Dao
             string sql = "UPDATE AS_STATEMANAGER_SCANNER SET ROW_INDEX = {0} WHERE STATECODE = '{1}'";
             sql = string.Format(sql, this.index, stateItemCode);
             ExecuteNonQuery(sql);
-            Logger.Info(string.Format("{0} 号扫码器，校正完成,流水号：{1}", stateItemCode, index));
             result = true;
             return result;
         }
